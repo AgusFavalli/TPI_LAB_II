d10a7bb25d6e15e67ca513ad6ad489757e6d5983
Mauricio Doldan <mauricio_doldan@hotmail.com>
2024-06-17 22:17:57 -0300

    modificacion Vistafichamedica

--- vista/VistaFichaMedica.py
+++ vista/VistaFichaMedica.py
@@ -4,6 +4,7 @@ class VistaFichaMedica:
         print("Menú de Gestión de Razas\n"
             "1- Consultar una ficha medica\n"
             "2- Modificar una Ficha Medica\n"
+            "3- Crear una ficha médica \n"            
             "5- Volver\n")
         return input("Seleccione una opción: ")
    
@@ -21,6 +22,18 @@ class VistaFichaMedica:
         for i in lista:
             print(i)
 
+    def mostrarFichaMedica(self, fichaMedica):
+        if fichaMedica:
+            print(f"Fecha: {fichaMedica['fecha']}")
+            print(f"Tratamiento: {fichaMedica['tratamiento']}")
+            print(f"Veterinario: {fichaMedica['veterinario']}")
+            print(f"Diagnósticos: {fichaMedica['diagnosticos']}")
+            print(f"Vacunas: {fichaMedica['vacunas']}")
+        else:
+            print("No se encontró la ficha médica.")
+
+
+
     def obtenerFichaMedica(self):
         return input("ingrese el nombre de la ficha medica")
 
@@ -37,12 +50,19 @@ class VistaFichaMedica:
     def ingresarFecha(self):
         return input("Ingrese la fecha de la ficha médica: ")
     
-    def ingresarInformacion(self):
+    def solicitarDatosFichaMedica(self):
+        fecha = input("Ingrese la fecha: ")
         tratamiento = input("Ingrese el tratamiento: ")
         veterinario = input("Ingrese el nombre del veterinario: ")
-        diagnostico = input("Ingrese el diagnóstico: ")
+        diagnosticos = input("Ingrese los diagnósticos: ")
         vacunas = input("Ingrese las vacunas: ")
-        return tratamiento, veterinario, diagnostico, vacunas  
+        return {
+            'fecha': fecha,
+            'tratamiento': tratamiento,
+            'veterinario': veterinario,
+            'diagnosticos': diagnosticos,
+            'vacunas': vacunas
+        }
     
     def mensajeModificacion(nombreMascota):
         print(f"\nFicha médica de {nombreMascota} actualizada correctamente.")
